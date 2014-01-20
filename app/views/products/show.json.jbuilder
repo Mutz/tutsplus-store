a58ca9fbf94a4024186b4807284199c15cfc41f1
José Mota <jose@josemota.net>
2014-01-20 11:56:29 +0000

    Checkpoint.

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :title, :description, :category, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'a0ab4e855e2055b1d4891e72f1427e3031fa1c0ebbb502cefd4f9113785414daa73f80e0113bdacd06550b1f6b6fc53213dc3b32145a28961eb39b4536e38aca'
